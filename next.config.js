96b3402125e77994479e4aa574ae39eea5d26d09
hassansabbir <mahmoodsabbir3087@gmail.com>
2024-01-07 19:48:09 +0600

    elu

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['www.api.technicaltest.quadtheoryltd.com'],
+    domains: ["www.api.technicaltest.quadtheoryltd.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://your-vercel-app.vercel.app",
+          },
+        ],
+      },
+    ];
   },
 };
